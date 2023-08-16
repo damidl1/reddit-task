9d3b83c34cbf9acd538d3f959615f7f199b7d25f
Damiano <d.dilionardo@gmail.com>
2023-08-16 21:42:42 +0200

    modifiche

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const dialogButton = document.getElementById('dialog-btn');
-    dialogButton.addEventListener('click', () => {
-        const dialog = document.querySelector('dialog');
+    const dialogButton = document.getElementById('dialog-btn'); 
+    const dialog = document.querySelector('dialog');
+
+    if (localStorage.getItem('showDialog') === 'true') {
         dialog.showModal();
+        localStorage.removeItem('showDialog');
+    }
+
+    dialog.close();
+    
+    dialogButton.addEventListener('click', (event) => {
+       
+        const openDialog = event.target.getAttribute('data-open-dialog');
+        if (openDialog === 'true') {
+            dialog.showModal();
+        }
     });
 
 
     const submitButton = document.getElementById('submit-button');
     submitButton.addEventListener('click', () => {
+        localStorage.setItem('showDialog', 'false');
+
         const selectedSubreddits = Array.from(document.querySelectorAll('input[name="subreddit"]:checked'))
             .map(checkbox => checkbox.value);
 
@@ -18,3 +32,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
