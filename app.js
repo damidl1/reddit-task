b379d70581c7d7d3f76b130d7ed0a8686135cd89
Damiano <d.dilionardo@gmail.com>
2023-08-25 16:26:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const subredditList = document.querySelector('.subreddit-list');
+    const dialogButton = document.getElementById('dialog-btn'); 
+    const dialog = document.querySelector('dialog');
+    const cancelButton = document.getElementById('cancel-button');
+    const removeSubredditButton = document.getElementById('removesubreddit-btn');
+    const finishRemovingButton = document.getElementById('finish-removing-btn');
+    const addSubredditButton = document.getElementById('add-subreddit-button');
+    const submitButton = document.getElementById('submit-button');
+    const checkboxes = document.querySelectorAll('input[name="subreddit"]');
+
     function saveCheckboxState(checkbox) {
         const subreddit = checkbox.value;
         localStorage.setItem(`subreddit_${subreddit}`, checkbox.checked ? 'true' : 'false');
@@ -11,8 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
         checkbox.checked = isChecked;
     }
 
+    function addSubreddit(subreddit) {
+        const label = document.createElement('label');
+        label.innerHTML = `
+            <input type="checkbox" name="subreddit" value="${subreddit}">
+            <a href="https://www.reddit.com/r/${subreddit}" target="_blank">${subreddit}</a>
+        `;
+        subredditList.insertBefore(label, subredditList.firstChild);
+    }
 
-    const removeSubredditButton = document.getElementById('removesubreddit-btn');
     removeSubredditButton.addEventListener('click', () => {
         const redditHeader = document.querySelector('reddit-header');
         redditHeader.showRemoveMode();
@@ -20,43 +37,19 @@ document.addEventListener('DOMContentLoaded', () => {
         finishRemovingButton.classList.remove('hide-remove-buttons');
     });
 
-    const addSubredditButton = document.getElementById('add-subreddit-button');
     addSubredditButton.addEventListener('click', () => {
         const newSubredditInput = document.getElementById('new-subreddit-input');
         const newSubreddit = newSubredditInput.value.trim();
 
         if (newSubreddit !== '') {
-            const subredditList = document.querySelector('.subreddit-list');
-            const label = document.createElement('label');
-            label.innerHTML = `
-            <input type="checkbox" name="subreddit" value="${newSubreddit}">
-            <a href="https://www.reddit.com/r/${newSubreddit}" target="_blank">${newSubreddit}</a>
-            `;
-            subredditList.insertBefore(label, subredditList.firstChild);
+            addSubreddit(newSubreddit);
             const savedSubreddits = JSON.parse(localStorage.getItem('subreddits')) || [];
             savedSubreddits.push(newSubreddit);
             localStorage.setItem('subreddits', JSON.stringify(savedSubreddits));
-            
             newSubredditInput.value = '';
         }
     });
 
-    // const savedSubreddits = JSON.parse(localStorage.getItem('subreddits')) || [];
-    // savedSubreddits.forEach(subreddit => {
-    //     const subredditList = document.querySelector('.subreddit-list');
-    //     const label = document.createElement('label');
-    //     label.innerHTML = `
-    //     <input type="checkbox" name="subreddit" value="${newSubreddit}">
-    //     <a href="https://www.reddit.com/r/${newSubreddit}" target="_blank">${newSubreddit}</a>
-    //     `
-    //     subredditList.insertBefore(label, subredditList.firstChild);
-    // })
-
-    const dialogButton = document.getElementById('dialog-btn'); 
-    const dialog = document.querySelector('dialog');
-    const cancelButton = document.getElementById('cancel-button');
-
-    const checkboxes = document.querySelectorAll('input[name="subreddit"]');
     checkboxes.forEach(restoreCheckboxState);
 
     if (localStorage.getItem('showDialog') === 'true' && localStorage.getItem('subreddits') === null) {
@@ -69,33 +62,27 @@ document.addEventListener('DOMContentLoaded', () => {
     cancelButton.addEventListener('click', () => {
         dialog.close();
     });
-    
+
     dialogButton.addEventListener('click', (event) => {
-       const openDialog = event.target.getAttribute('data-open-dialog');
+        const openDialog = event.target.getAttribute('data-open-dialog');
         if (openDialog === 'true') {
             dialog.showModal();
         }
     });
 
-    const finishRemovingButton = document.getElementById('finish-removing-btn');
     finishRemovingButton.addEventListener('click', () => {
         const redditHeader = document.querySelector('reddit-header');
         redditHeader.removeAttribute('remove-mode');
         removeSubredditButton.classList.remove('hide-remove-buttons');
         finishRemovingButton.classList.add('hide-remove-buttons');
+    });
 
-    })
-
-
-
-    const submitButton = document.getElementById('submit-button');
     submitButton.addEventListener('click', () => {
         localStorage.setItem('showDialog', 'false');
-
         const selectedSubreddits = Array.from(document.querySelectorAll('input[name="subreddit"]:checked'))
             .map(checkbox => checkbox.value);
 
-            selectedSubreddits.forEach(saveCheckboxState);
+        selectedSubreddits.forEach(saveCheckboxState);
 
         if (selectedSubreddits.length > 0) {
             const redditHeader = document.querySelector('reddit-header');
@@ -103,17 +90,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const subredditList = document.querySelector('.subreddit-list');
-   checkboxes.forEach(restoreCheckboxState);
     const savedSubreddits = JSON.parse(localStorage.getItem('subreddits')) || [];
     savedSubreddits.forEach(subreddit => {
-        
         const label = document.createElement('label');
         label.innerHTML = `
-        <input type="checkbox" name="subreddit" value="${newSubreddit}">
-        <a href="https://www.reddit.com/r/${newSubreddit}" target="_blank">${newSubreddit}</a>
+        <input type="checkbox" name="subreddit" value="${subreddit}">
+        <a href="https://www.reddit.com/r/${subreddit}" target="_blank">${subreddit}</a>
         `
         subredditList.insertBefore(label, subredditList.firstChild);
-    })
-});
+    });
 
+});
